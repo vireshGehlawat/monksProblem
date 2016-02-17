b1ddf4df7d428cfd833cb0228f59ba8b8b376f7a
vireshGehlawat <viresh.gehlawat@flipkart.com>
2016-02-17 19:47:56 +0530

    Added comments in Classifier

--- src/main/java/models/Classifier.java
+++ src/main/java/models/Classifier.java
@@ -8,11 +8,17 @@ import java.util.List;
 /**
  * @author viresh.gehlawat
  */
-
-/**
- *
- */
 public interface Classifier {
+	/**
+	 * Trains a classifier. Must not change the input data set.
+	 * @param inputDataList Input training data set to train the classifier.
+	 * @param features List of features for this classifier.
+	 */
 	public void train(List<DataRow> inputDataList, List<Feature> features);
+
+	/**
+	 * @param dataRow input data set to be predicted
+	 * @return prediction
+	 */
 	public Boolean predict(DataRow dataRow);
 }
